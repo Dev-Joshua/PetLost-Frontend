9eda6391cfae4b2aa4cfb87273c0aecbebe15916
Joshua Daniel <Joshua_dup@hotmail.com>
2022-09-22 17:45:09 -0500

    añado ventana emergente a la primer card del home

---
+++ Frontend/JavaScript/modal.js
@@ -0,0 +1,13 @@
+const contactar = document.getElementById('contactar')
+const modal_container = document.getElementById('modal-container')
+const close = document.getElementById('close')
+
+contactar.addEventListener('click', () => {
+  modal_container.classList.add('show')
+  // alert('prueba')
+})
+
+close.addEventListener('click', () => {
+  modal_container.classList.remove('show')
+  // alert('prueba')
+})
